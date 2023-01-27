ff614314c35a65da0cdba9fa474391a87f6ee925
Uchley <mkwiecinski97@gmail.com>
2023-01-27 09:57:24 +0100

    App5 bugfix

--- App5/app/src/main/java/com/example/app5/SecondActivity.java
+++ App5/app/src/main/java/com/example/app5/SecondActivity.java
@@ -1,30 +1,22 @@
+
 package com.example.app5;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-
-public class SecondActivity extends AppCompatActivity {
-
-
-
-    @SuppressLint("SetTextI18n")
+public class SecondActivity extends Activity {
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_second);
-
-
-        String user = "John";
+        String user = "";
         String gift = "How are you?";
 
         user = getIntent().getExtras().getString("username");
         gift = getIntent().getExtras().getString("gift");
 
-
         TextView infoTextView = findViewById(R.id.textView);
-        infoTextView.setText((user +", send " + gift));
+        infoTextView.setText((user + ", send " + gift));
     }
 }
